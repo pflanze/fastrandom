84a746679130af865ff9fd04b39b2bc2860b97f5
Christian Jaeger <chrjae@gmail.com>
2011-06-02 21:33:38 -0400

    initialize from /dev/urandom

--- test-CMWC.c
+++ test-CMWC.c
@@ -3,13 +3,35 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "CMWC.c"
 
 #define BUFLEN 1024
 
+
+void initQ () {
+    int fd= open("/dev/urandom", O_RDONLY);
+    if (fd<0) {
+	perror("open");
+	exit(1);
+    }
+    {
+	ssize_t res;
+	res= read(fd, Q, sizeof(Q));
+	if (res != sizeof(Q)) {
+	    perror("read");
+	    exit(1);// ah I wanted a return value for that?
+	}
+    }
+}
+
 int main () {
     unsigned long buf[BUFLEN];
-    
+
+    initQ();
     while(1) {
 	{
 	    int i;
